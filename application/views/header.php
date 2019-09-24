9553045d38840d120fc05ae89ef7296a670cdffd
milenarmp <milenarmp@gmail.com>
2019-09-24 09:07:24 -0300

    no message

--- application/views/header.php
+++ application/views/header.php
@@ -1,7 +1,14 @@
 	<body>
-		<div class="container-fluid" style="background-color: blue">
+		<div class="container-fluid">
 		<div class="header">
-			<nav class="navbar navbar-dark navbar-fixed-top bg-dark">
-				<a class="navbar-brand" href="#">Início</a>
+			<nav class="navbar navbar-expand-lg navbar-fixed-top" style="background-image: linear-gradient(to right, #005100, #83f1a9);">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item">
+						<a class="navbar-brand" href="#" style="font-style: none">Início</a>
+					</li>
+					<li class="nav-item">
+						<a class="navbar-brand" href="#">Pesquisar EPIs</a>
+					</li>
+				</ul>
 			</nav><!-- fim da barra de navegação -->
 		</div>
