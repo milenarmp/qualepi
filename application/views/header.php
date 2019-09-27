954f082cc7c6d3bd7251fe904ca682128e71bbc4
milenarmp <milenarmp@gmail.com>
2019-09-27 10:53:33 -0300

    no message

--- application/views/header.php
+++ application/views/header.php
@@ -1,14 +1,42 @@
 	<body>
 		<div class="container-fluid">
 		<div class="header">
-			<nav class="navbar navbar-expand-lg navbar-fixed-top" style="background-image: linear-gradient(to right, #005100, #83f1a9);">
-				<ul class="navbar-nav mr-auto">
-					<li class="nav-item">
-						<a class="navbar-brand" href="#" style="font-style: none">Início</a>
+			<nav class="navbar navbar-expand-lg navbar-fixed-top" id="barra">
+				<ul class="navbar-nav" style="margin-right: 65%">
+					<li class="nav-item"  style="margin-right: 15px">
+						<a class="navbar-link" href="#"><span style="color: white">Início</span></a>
 					</li>
-					<li class="nav-item">
-						<a class="navbar-brand" href="#">Pesquisar EPIs</a>
+					<li class="nav-item" style="margin-right: 15px">
+						<a class="navbar-link" href="#"><span style="color: white">Pesquisar EPIs</span></a>
+					</li>
+					<li class="nav-item" style="margin-right: 15px">
+						<a class="navbar-link" href="#"><span style="color: white">Sobre Nós</span></a>
+					</li>
+					<li class="nav-item" style="margin-right: 15px">
+						<a class="navbar-link" href="#"><span style="color: white">Contato</span></a>
 					</li>
 				</ul>
+				<span class="navbar-text">Cadastro | Login </span>
 			</nav><!-- fim da barra de navegação -->
+			<div class="teste" style="">
+				<form>
+					<div class="form-group">
+						<div class="col-md-6 offset-md-1">
+							<input type="text" class="form-conrol" id="pesquisa">
+						</div>
+					</div>
+					<div class="form-group" id="buscar">
+						<div class="col-md-6 offset-md-1" id="filtros">
+							<label>Pesquisar por:</label>
+							<div class="btn-group" role="group" aria-label="Filtros" style="margin-bottom: 5px">
+								<button type="button" class="btn btn-success btn-sm">nº CA</button>
+								<button type="button" class="btn btn-success btn-sm">Fabricante</button>
+								<button type="button" class="btn btn-success btn-sm">Descrição</button>
+								<button type="button" class="btn btn-success btn-sm">Risco</button>
+								<button type="button" class="btn btn-success btn-sm">Customização de Filtros</button>
+							</div>
+						</div>
+					</div>
+				</form>
+			</div>
 		</div>
